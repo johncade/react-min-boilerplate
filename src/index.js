a7d838c07dc9a11a04fe60c39e7eb11b60493c0f
JC Griffin <johncade@kindhealth.co>
2018-12-08 21:55:39 -0600

    initial boilerplate

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Welcome to my React Boilerplate: Minimum Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
